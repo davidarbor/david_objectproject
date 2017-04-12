ed2c63340d3481629e7fd50e102aa097932b2c3d
David Arbor <davidarbor@students.berkeley.net>
2017-04-12 10:35:12 -0700

    made new js window

---
+++ extraeffects.js
@@ -0,0 +1,5 @@
+/**
+ * Created by h205p2 on 4/12/17.
+ */
+var scaldArray=["burn", "burn", "burn", "no burn", "no burn", "no burn", "no burn", "no burn", "no burn", "no burn"];
+var scaldIdx = Math.floor(Math.random() * scaldArray.length);
